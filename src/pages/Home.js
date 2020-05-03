f1aa76fc802ef444f6bcf19dc9fda1ad3479bacd
Alexandru Prisacariu <zalexxanderx@gmail.com>
2020-05-03 14:55:28 +0300

    Testing the video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 import "../style/Home.scss";
 
@@ -9,9 +10,16 @@ class HomePage extends React.Component {
             <div id="homePage" className="page">
                 <div className="_left">
                     <img className="_logo" src="/imgs/logo.png"/>
+                    <div className="_menuCnt">
+                        <Link className="_blog" to="#">Blog</Link>
+                        <Link className="_projects" to="#">Projects</Link>
+                        <Link className="_aboutMe" to="#">About me</Link>
+                    </div>
                 </div>
                 <div className="_right">
-
+                    <video autoPlay loop preload="auto" muted>
+                        <source src="/imgs/home_video.mp4" type="video/mp4"/>
+                    </video>
                 </div>
             </div>
         )
